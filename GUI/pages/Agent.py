d46d661aead8856181d1bc8d76c38b8e89343f18
JordyBacherot <jordy.bacherot@outlook.fr>
2024-09-04 21:58:29 +0200

    Create an agent with groq, with key, model

--- GUI/pages/Agent.py
+++ GUI/pages/Agent.py
@@ -11,22 +11,26 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 # Importer la fonction depuis ToJson.py
-from Utils.Tojson import get_key, add_key
+from Utils.Tojson import get_key, add_key, create_an_agent
 
 @st.dialog("Create an agent with Groq")
 def dialog_create_groq_agent():
     key = get_key("groqkey")
-    col1, col2 = st.columns(2)
     if key is not None:
-        st.session_state.key = selectbox("Choose a key", key)
+        st.session_state.key = selectbox("Choose a key name", key)
+        st.button("Refresh the keys")
+    else :
+        st.write("You have to add a key")
     st.divider()
     st.write("If you want to add a key :")
     st.session_state.newkeyname = st.text_input("Please enter your name key")
     st.session_state.newkey = st.text_input("Please enter your key")
-    col1, col2 = st.columns(2)
     if st.button("Add the key") :
-        add_key("groqkey", st.session_state.newkeyname, st.session_state.newkey)
-        st.rerun()
+        verify = add_key("groqkey", st.session_state.newkeyname, st.session_state.newkey)
+        if not verify:
+            st.write("This key name already exist")
+        else :
+            st.write("The key has been added")
     if st.button("Where can I find my key ?") :
         st.info("You have to create an account on the website Groq and go to the API section to get your key")
     st.divider()
@@ -37,8 +41,13 @@ def dialog_create_groq_agent():
         "mixtral-8x7b-32768",
     ]
     st.session_state.model = st.selectbox("Please enter the model you want to use", model_ids)
+    st.session_state.agentname = st.text_input("Please enter the name of your agent")
     if st.button("Create the agent"):
-        st.session_state.dialog.close()
+        verify = create_an_agent("groq", st.session_state.agentname, st.session_state.key, st.session_state.model)
+        if verify is not None:
+            st.rerun()
+        else :
+            st.warning("An agent with this name already exist")
 
 if st.button("Click here to add a Groq agent") :
     dialog_create_groq_agent()
--- Utils/Tojson.py
+++ Utils/Tojson.py
@@ -1,11 +1,12 @@
 import json
+import os
 
 
 # look if a dictionary groqkey is present in yourkeys.json, if present return the dictionnary
 def get_key(apibase):
     with open('YourAgents/yourkeys.json', 'r') as file:
         data = json.load(file)
-        if apibase in data.keys():
+        if len(data[apibase]) != 0:
             return data[apibase]
         else:
             return None
@@ -13,11 +14,20 @@ def get_key(apibase):
 def add_key(apibase, keyname, key):
     with open('YourAgents/yourkeys.json', 'r') as file:
         data = json.load(file)
+        # verify if the key already exist
+        if keyname in data[apibase]:
+            return False
         data[apibase][keyname] = key
     with open('YourAgents/yourkeys.json', 'w') as file:
         json.dump(data, file, indent=4)
+    return True
 
 def create_an_agent(apibase, agentname, agentkey, agentmodel):
-    data = {"agentname" : agentname, "apibase" : apibase, "agentkey" : agentkey, "agentmodel" : agentmodel}
-    with open(f'YourAgents/{agentname}.json', 'w') as file:
+    # verify if the file already exist
+    if not os.path.exists(f'YourAgents/Agents/{agentname}.json'):
+        return "This agent already exist"
+    data = {"agentname" : agentname, "apibase" : apibase, "agentkey" : agentkey, "agentmodel" : agentmodel, "historique" : []}
+    with open(f'YourAgents/Agents/{agentname}.json', 'w') as file:
         json.dump(data, file, indent=4)
+    return None
+
